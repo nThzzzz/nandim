26c7aaf903f23e246462ce4ded6096ea7455c060
Fernando Bordin <fernandobordin1234@gmail.com>
2024-10-14 22:38:25 -0300

    Update manip_matrizes.h

--- manip_matrizes.h
+++ manip_matrizes.h
@@ -6,9 +6,16 @@
 
 // Estrutura para representar uma fração
 typedef struct Fracao {
-    int numerador;
-    int denominador;
+  int numerador;
+  int denominador;
 } Fracao;
+typedef struct {
+  char CPF[12];
+  char senha[7];
+  char nome[100];
+  FILE *historico;
+  char nome_hist[100];
+} pessoa;
 
 Fracao simplificar(Fracao f);
 Fracao somar(Fracao a, Fracao b);
@@ -17,13 +24,29 @@ void imprimirFracao(Fracao f);
 Fracao lerFracao();
 int mdc(int a, int b);
 
-void printMatriz(Fracao** matriz, int linhas, int colunas);
-void transoposta(Fracao** matriz, int linhas, int colunas);
-void soma(Fracao** matriz, int linhas, int colunas);
-void sub(Fracao** matriz, int linhas, int colunas);
-void multi(Fracao** matriz, int linhas, int colunas);
-Fracao determinante(Fracao** matriz, int n);
-void inversa(Fracao** matriz, int n);
-void decomposicaoLU(Fracao** matriz, int n);
+void printMatriz(Fracao **matriz, int linhas, int colunas);
+void transoposta(Fracao **matriz, int linhas, int colunas);
+void soma(Fracao **matriz, int linhas, int colunas);
+void sub(Fracao **matriz, int linhas, int colunas);
+void multi(Fracao **matriz, int linhas, int colunas);
+Fracao determinante(Fracao **matriz, int n);
+void inversa(Fracao **matriz, int n);
+void decomposicaoLU(Fracao **matriz, int n);
+void menu(pessoa pessoas[], int usuariologado, int *linhas, int *colunas);
+int arquivoexiste(const char *filename);
+void escrever(pessoa pessoas[], int quantidade);
+void ler(FILE *file, pessoa pessoas[], int quantidade);
+void login(pessoa pessoas[], int usuariologado, int *linhas, int *colunas);
+void cadastrar(pessoa pessoas[], int usuariologado, int *linhas, int *colunas);
+void menuinicial(pessoa pessoas[], int usuariologado, int *linhas,int *colunas);
+void limpaterminal();
+void espera();
+void limpabuffer();
+int verificaCPF(char *cpf);
+Fracao **criarMatriz(int *linhas, int *colunas);
+void limparMatriz(Fracao **matriz, int linhas);
+void criaTXTHISTORICO(FILE *file,int usuariologado,pessoa pessoas[]);
+void escrevehistorico(int usuariologado,pessoa pessoas[]);
+void lerhistorico(int usuariologado, pessoa pessoas[]);
 
 #endif
